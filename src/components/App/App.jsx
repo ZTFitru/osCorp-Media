ad71a3c3e4421757a8a8aeec53fe34c9f3dbdfe6
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-09-27 14:53:08 -0400

    Created a mock data.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,16 @@
 import Navbar from '../Navbar/Navbar'
 import './App.css'
+import NewsData from '../../MockData.json'
+import Titlepage from '../Titlepage/Titlepage'
 
 const App = ()=> {
+
+    const myNewsData = NewsData.articles
 
     return (
         <main>
             <Navbar />
+            <Titlepage myNewsData={myNewsData} />
         </main>
     )
 }
--- src/components/Titlepage/Titlepage.jsx
+++ src/components/Titlepage/Titlepage.jsx
@@ -1,10 +1,18 @@
 import './Titlepage.css'
 
-const Titlepage = ()=> {
+const Titlepage = ({ myNewsData })=> {
+
+    console.log(myNewsData)
 
     return (
         <div>
-
+            {myNewsData.map((news, index)=> {
+                return <div key={index}>
+                    <h2>{news.title}</h2>
+                    <img src={news.urlToImage} alt='' />
+                    <p>{news.publishedAt}</p>
+                </div>
+            })}
         </div>
     )
 }
